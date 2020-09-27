79ac26d75b69d24393c10e93a79fb0609cbb7265
Julio <jh.juliohenrique@gmail.com>
2020-09-27 18:04:33 -0300

    fix initial CRUD

---
+++ APLICATION_LIBRARY_PRECISA/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('get_books',views.get_books,name='get_books'),
+    path('',views.index,name='index'),
+]
--- APLICATION_LIBRARY_PRECISA/views.py
+++ APLICATION_LIBRARY_PRECISA/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Book_description
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def get_books(request):
+
+    books = Book_description.objects.all()
+
+    print("\n")
+    print(list(books.values()))
+    print("\n")
+
+    return JsonResponse({"teste":1})
+
+def index(request):
+
+    books = Book_description.objects.all()
+
+    print("\n")
+    print(list(books.values()))
+    print("\n")
+
+    return JsonResponse({"teste":1})
+
+
+
